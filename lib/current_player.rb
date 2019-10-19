ddcb2c11981ac849f1cab9dba2dd72a9229167d1
vy vo <tuonggvy.vo@gmail.com>
2019-10-19 02:34:42 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,4 +9,12 @@ def turn_count(board)
   counter
 end
 
-#
+#current_player
+def current_player(board)
+  if counter % 2 = 0
+    board[index] = "X"
+  end
+  if current counter % 2 = 1
+    board[index] = "O"
+  end
+end
