02d0053c50326dec79b37338349e7e2c955858e0
vy vo <tuonggvy.vo@gmail.com>
2019-10-19 02:23:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,8 @@
 board = [" "," "," "," "," "," "," "," "," "]
 counter = 0
 def turn_count(board)
-  board.each do |counter|
-    if board[counter] == "X" || board[index] == "O"
+  board.each do |index|
+    if board[index] == "X" || board[index] == "O"
       counter += 1
     end
   end
